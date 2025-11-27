c4cfebcaa837d6d42cef09eee94d7856c11caf1a
yangruoxuan0516 <yangruoxuan0516@users.noreply.github.com>
2025-11-27 14:48:48 +0000

    deploy: cc85a2b0bf2c71e7b410162b9f1c13f0f9234456

---
+++ plausible-5bcaa6e3.js
@@ -0,0 +1,16 @@
+(function () {
+  // 引入 Plausible 的站点专属脚本
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = "https://plausible.io/js/pa-bG2-GOmUI61_egH6HAcMY.js"; 
+  document.head.appendChild(s);
+
+  // 初始化 plausible 对象（照抄官方给你的逻辑）
+  window.plausible = window.plausible || function () {
+    (plausible.q = plausible.q || []).push(arguments);
+  };
+  plausible.init = plausible.init || function (i) {
+    plausible.o = i || {};
+  };
+  plausible.init();
+})();
