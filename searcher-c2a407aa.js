e214f8e231988ad0fd8e68b61b81ed05b9990d33
yangruoxuan0516 <yangruoxuan0516@users.noreply.github.com>
2026-03-17 06:53:09 +0000

    deploy: e03faaff111e9b96135534f5aac7fc48a0a92708

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-11f91945.js',
+                path_to_root + 'searchindex-764f08a3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
